0f3228581045f7350b833ca05c523294f480bbaf
RiptCodes <matyika1968@gmail.com>
2026-07-29 20:36:50 +0100

    load ROM into memory at 0x200

--- main.py
+++ main.py
@@ -9,37 +9,46 @@ screen = pygame.display.set_mode((640, 320))
 clock = pygame.time.Clock()
 running = True
 
+# CPU / Memory
+memory = bytearray(4096)
+V = bytearray(16)
+I = 0
+pc = 0x200
+stack = []
+
+# log message
+def log(text_to_log):
+    pass
+
+# ROM Load
+with open("roms/test_opcode.ch8", 'rb') as f:
+    rom = f.read()
+    memory[pc:pc + len(rom)] = rom #assigning ROM to the Memory location 0x200 - end of ROM 0xFFF
+
+
+print(memory[pc:pc+20].hex())
+
 framebuffer = []
 for _ in range(32):
     row = [0] * 64
     framebuffer.append(row)
 
 
-for i in range(32):
-    framebuffer[i][i] = 1
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
-    screen.fill("black")
+#     screen.fill("black")
 
-    for y in range(0,32):
-        for x in range(0,64):
-            if framebuffer[y][x] == 1:
-                pygame.draw.rect(screen, (0,255,0), (x * SCALE, y * SCALE, SCALE, SCALE))
+#     for y in range(0,32):
+#         for x in range(0,64):
+#             if framebuffer[y][x] == 1:
+#                 pygame.draw.rect(screen, (0,255,0), (x * SCALE, y * SCALE, SCALE, SCALE))
 
 
-    pygame.display.flip()
-    clock.tick(60)
+#     pygame.display.flip()
+#     clock.tick(60)
 
-pygame.quit()
+# pygame.quit()
 
-
-# Debugging
-with open("roms/test_opcode.ch8", 'rb') as f:
-    rom = f.read()
-
-print(f"{len(rom)} bytes")
-print(rom[:20].hex())
